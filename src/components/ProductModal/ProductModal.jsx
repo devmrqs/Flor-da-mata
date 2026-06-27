2df6171ecff826144b097fe4fb782a5758b68d63
over <overboboto@gmail.com>
2026-06-27 01:08:55 -0300

    feat: adiciona fechamento do modal por ESC e clique externo

--- src/components/ProductModal/ProductModal.jsx
+++ src/components/ProductModal/ProductModal.jsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import styles from "./ProductModal.module.css";
 
 const ProductModal = ({ product, onClose }) => {
+  useEffect(() => {
+    if (!product) return;
+    function handleEscKey(event) {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    }
+
+    document.addEventListener("keydown", handleEscKey);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [product, onClose]);
   if (!product) return null;
 
   return (
-    <div className={styles.overlay}>
-      <div className={styles.modal}>
+    <div className={styles.overlay} onClick={onClose}>
+      <div
+        className={styles.modal}
+        onClick={(event) => event.stopPropagation()}
+      >
         <button
           className={styles.closeButton}
           onClick={onClose}
