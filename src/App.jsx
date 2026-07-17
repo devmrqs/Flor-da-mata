830a36aee339c00b87c0d6d5973aba06acd2f66f
over <overboboto@gmail.com>
2026-07-16 21:33:09 -0300

    feat: configura ScrollSmoother e ajusta estilos do Button, ProductModal e Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 // Libraries
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 // CSS
 import styles from "./App.module.css";
@@ -14,19 +19,42 @@ import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Products from "./pages/Products/Products";
 import Partnership from "./pages/Partner/Partnership";
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 function App() {
+  const smootherRef = useRef(null);
+
+  useGSAP(() => {
+    smootherRef.current = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.2,
+      effects: true,
+    });
+
+    ScrollTrigger.refresh();
+
+    return () => {
+      smootherRef.current?.kill();
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
       <div className={styles.containerHeader}>
         <Navbar />
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/produtos" element={<Products isPage />} />
-        <Route path="/sobre" element={<About />} />
-        <Route path="/seja-parceiro" element={<Partnership />} />
-      </Routes>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/produtos" element={<Products isPage />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/seja-parceiro" element={<Partnership />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
